edb09654c426f0a1745071dd290f9f37f885b6f4
renan <renan.xote@hotmail.com>
2024-06-28 17:38:45 -0300

    feat "adding navbar with bootstrap, with another html archive that 'talks' to the others'"

---
+++ mysite/myapp/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path,include
+from myapp import views
+
+app_name= 'myapp'
+
+urlpatterns = [
+    path('', views.index,name='index'),
+    path('products/', views.products,name='products'),
+    path('book/<int:book_id>/', views.details, name='details'),
+]
+    
+
