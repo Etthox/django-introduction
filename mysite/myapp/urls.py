e2a4e3fdb816ae0574705bf0dc32838ae3876858
renan <renan.xote@hotmail.com>
2024-07-01 16:39:00 -0300

    feat: "adding a page to add new books and saving throught a post method"

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.index,name='index'),
     path('products/', views.products,name='products'),
     path('book/<int:book_id>/', views.details, name='details'),
+    path('add/', views.add_book, name='add_book')
 ]
     
 
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Book
 
 
@@ -16,3 +16,22 @@ def details(request,book_id):
 
 def products(request):
     return HttpResponse('There are some Products:')
+
+def add_book(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        
+        if 'book_image' in request.FILES:
+            book_image = request.FILES['book_image']
+        else:
+            book_image = None  
+
+        book = Book(name=name, description=description, price=price, book_image=book_image)
+
+        book.save()
+
+        return HttpResponseRedirect('/')  
+
+    return render(request, 'myapp/add_book.html')
